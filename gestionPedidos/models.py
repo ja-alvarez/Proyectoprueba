921201bb44203e339cef316d7ed335d48e3513c0
Juan Alvarez <juan.alvarez1989@gmail.com>
2021-02-18 11:07:43 -0300

    first commit

---
+++ gestionPedidos/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Clientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField()
+    telefono=models.CharField(max_length=12)
+
+class Articulos(models.Model):
+    nombre=models.CharField(max_length=30)
+    seccion=models.CharField(max_length=20)
+    precio=models.IntegerField()
+
+    def __str__(self):
+        return 'Nombre articulo: %s, seccion: %s, precio: %s' %(self.nombre, self.seccion, self.precio)
+
+class Pedidos(models.Model):
+    numero=models.IntegerField()
+    fecha=models.DateField()
+    entregado=models.BooleanField()
